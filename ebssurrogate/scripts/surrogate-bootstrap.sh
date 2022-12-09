019f969ab30a64d517fce1e8cbf8c324d7ca1ec5
Paul Cioanca <paul.cioanca@supabase.io>
2022-12-09 20:18:51 +0200

    chore: attempting to pass env yet again

--- ebssurrogate/scripts/surrogate-bootstrap.sh
+++ ebssurrogate/scripts/surrogate-bootstrap.sh
@@ -205,8 +205,7 @@ EOF
 	# Run Ansible playbook
 	#export ANSIBLE_LOG_PATH=/tmp/ansible.log && export ANSIBLE_DEBUG=True && export ANSIBLE_REMOTE_TEMP=/mnt/tmp 
 	export ANSIBLE_LOG_PATH=/tmp/ansible.log && export ANSIBLE_REMOTE_TEMP=/mnt/tmp 
-	export CCACHE_DIR=/mnt/tmp/ccache
-	ccache -s
+	export CCACHE_DIR=/tmp/ccache
 	ansible-playbook -c chroot -i '/mnt,' /tmp/ansible-playbook/ansible/playbook.yml --extra-vars " $ARGS"
 }
 
